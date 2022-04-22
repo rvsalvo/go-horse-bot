3cd9867954841bda3ce6b1d1d736c40996785054
rsalvo <rsalvo.be@gmail.com>
2022-04-22 23:09:09 +0200

    Added commands

--- src/main.py
+++ src/main.py
@@ -5,9 +5,17 @@ import json
 import re
 
 from dotenv import load_dotenv
+from discord.ext import commands
+
 load_dotenv()
 
-client = discord.Client()
+help_command = commands.DefaultHelpCommand(
+    no_category = 'Commands'
+)
+
+client = commands.Bot(command_prefix="$", description="Hey there, I'm the GoHorse bot!", help_command=help_command)
+
+## client = discord.Client()
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -15,18 +23,21 @@ def get_quote():
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return(quote)
 
+@client.command()
+async def hello(ctx):
+  await ctx.send("Hello " + ctx.author.name + "!")
+
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
+  await client.change_presence(activity=discord.Game('GoHorse'))
 
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send("Hello " + message.author.name + "!")
-  elif message.content.startswith('$inspireMe'):
+  if message.content.startswith('$inspireme') or message.content.startswith('$inspireMe'):
     quote = get_quote()
     await message.channel.send(quote)
   else:
@@ -34,6 +45,6 @@ async def on_message(message):
     result = re.search("0x([a-fA-F]|[0-9]){40}", message.content)
     if result:
         await message.channel.purge(limit=1)
-  
+    await client.process_commands(message)
 
 client.run(os.environ['TOKEN'])
